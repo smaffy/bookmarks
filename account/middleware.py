62c75cc0c44e6f878c3b13c1a93f0c3c4d4271f6
smaffy <kate@itpeople.ee>
2019-05-04 10:35:54 +0200

    4. MySocialAuthExceptionMiddleware. AuthAlreadyAssociated (ne otvalivaetsja ostalnoe)

--- account/middleware.py
+++ account/middleware.py
@@ -1,12 +1,12 @@
+
+from django.shortcuts import render
 from social_django.middleware import SocialAuthExceptionMiddleware
-from social_core import exceptions as social_exceptions
-from django.http import HttpResponse
+from social_core.exceptions import AuthAlreadyAssociated
 
 
 class MySocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
     def process_exception(self, request, exception):
-        if hasattr(social_exceptions, exception.__class__.__name__):
-            # Here you can handle the exception as you wish
-            return HttpResponse('Exception: "%s", while processing your social account.' % exception)
+        if isinstance(exception, AuthAlreadyAssociated):
+            return render(request, "account/helpAuthAlreadyAssociated.html", {})
         else:
-            return super(MySocialAuthExceptionMiddleware, self).process_exception(request, exception)
+            pass
