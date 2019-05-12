b0a2e0934af422963a9a22ac3a694f8cc2bf892c
smaffy <kate@itpeople.ee>
2019-05-12 12:26:40 +0200

    8.3 bookmarklet (jQuery). + view for img + tokens + like/unlike(ajax) + decor

--- images/views.py
+++ images/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 from .models import Image
 from .forms import ImageCreateForm
+from common.decorators import ajax_required
 
 
 @login_required
@@ -35,3 +38,22 @@ def image_detail(request, id, slug):
     return render(request,
                   'images/image/detail.html',
                   {'section': 'images', 'image': image})
+
+
+@ajax_required
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status':'ok'})
+        except:
+            pass
+    return JsonResponse({'status': 'ko'})
